744567c653dbf29eb0e1120fcee205f7fbca7454
Your Name <bhasvitakatragadda@gmail.com>
2026-08-12 12:37:06 +0530

    Week 2 changes

---
+++ track/M02-Introduction_to_python/T01-Introduction-yo-python-basics/src/weekly_study_hours_function.py
@@ -0,0 +1,8 @@
+def display_study_hours():
+    hours = int(input())
+    days = int(input())
+    total = hours*days
+    print(f"Total Study Hours: {total}")
+
+# Call the function
+display_study_hours()
