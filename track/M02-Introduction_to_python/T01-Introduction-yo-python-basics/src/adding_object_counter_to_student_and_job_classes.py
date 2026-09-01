288210a9fcf4ad2e5eecfdab2c67343ecba7c40d
Your Name <bhasvitakatragadda@gmail.com>
2026-09-01 13:03:52 +0530

    Class variables

---
+++ track/M02-Introduction_to_python/T01-Introduction-yo-python-basics/src/adding_object_counter_to_student_and_job_classes.py
@@ -0,0 +1,25 @@
+class StudentProfile:
+    student_count = 0
+
+    def __init__(self):
+        StudentProfile.student_count += 1
+
+
+class JobDescription:
+    job_count = 0
+
+    def __init__(self):
+        JobDescription.job_count += 1
+
+
+n = int(input())
+m = int(input())
+
+for _ in range(n):
+    StudentProfile()
+
+for _ in range(m):
+    JobDescription()
+
+print(f"Students Created: {StudentProfile.student_count}")
+print(f"Jobs Created: {JobDescription.job_count}")
